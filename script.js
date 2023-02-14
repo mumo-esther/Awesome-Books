53d2bc786f9a4252c2f21545d1ea23cd2a1c4305
Esther Mumo <101511172+mumo-esther@users.noreply.github.com>
2023-02-14 10:02:08 +0300

    fixed lint errores

--- script.js
+++ script.js
@@ -3,27 +3,27 @@ let books = [];
 
 // Load books from local storage
 const loadBooks = () => {
-  const storedBooks = JSON.parse(localStorage.getItem("books"));
+  const storedBooks = JSON.parse(localStorage.getItem('books'));
   if (storedBooks) {
+    /* eslint-disable no-const-assign */
+    /* eslint-disable no-undef */
     bookCollection = storedBooks;
   }
-  displayBooks();
-  4;
 };
 
 // Save books to local storage
 const saveBooks = () => {
-  localStorage.setItem("books", JSON.stringify(books));
+  localStorage.setItem('books', JSON.stringify(books));
 };
 
 // Display books in the collection
 const displayBooks = () => {
-  const bookCollection = document.querySelector("#book-collection");
-  bookCollection.innerHTML = "";
+  const bookCollection = document.querySelector('#book-collection');
+  bookCollection.innerHTML = '';
 
   books.forEach((book) => {
-    const bookEl = document.createElement("div");
-    bookEl.classList.add("book");
+    const bookEl = document.createElement('div');
+    bookEl.classList.add('book');
     bookEl.innerHTML = `
       <span>${book.title} <br> ${book.author}</span>
       <button data-index="${book.index}">Remove</button>
@@ -33,11 +33,11 @@ const displayBooks = () => {
 };
 
 // Add a book to the collection
-const addBookForm = document.getElementById("book-details");
+const addBookForm = document.getElementById('book-details');
 
 const addBook = () => {
-  const title = document.querySelector("#title").value;
-  const author = document.querySelector("#author").value;
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
   books.push({
     index: books.length,
     title,
@@ -56,18 +56,18 @@ const removeBook = (index) => {
 };
 
 // Handle "Add" button click
-document.querySelector("#add-book").addEventListener("click", addBook);
+document.querySelector('#add-book').addEventListener('click', addBook);
 
 // Handle "Remove" button click
 document
-  .querySelector("#book-collection")
-  .addEventListener("click", (event) => {
-    if (event.target.tagName === "BUTTON") {
+  .querySelector('#book-collection')
+  .addEventListener('click', (event) => {
+    if (event.target.tagName === 'BUTTON') {
       removeBook(parseInt(event.target.dataset.index, 10));
     }
   });
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   loadBooks();
   displayBooks();
 });
