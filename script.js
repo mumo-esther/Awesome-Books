bf2981e579a311e93e425cc26071f2da7b21896c
Esther Mumo <101511172+mumo-esther@users.noreply.github.com>
2023-02-15 11:25:15 +0300

    a complete website with navigation

--- script.js
+++ script.js
@@ -46,3 +46,29 @@ document
   .getElementById('add-button')
   .addEventListener('click', bookCollection.addBook);
 bookCollection.displayBooks();
+
+// Get the navigation links and content sections
+const links = document.querySelectorAll('nav a');
+const sections = document.querySelectorAll('section');
+
+// Add click event listeners to the navigation links
+links.forEach((link) => {
+  link.addEventListener('click', () => {
+    // Remove the "active" class from all links
+    links.forEach((link) => link.classList.remove('active'));
+
+    // Add the "active" class to the clicked link
+    link.classList.add('active');
+
+    // Hide all sections
+    /* eslint-disable no-return-assign */
+    sections.forEach((section) => section.style.display = 'none');
+
+    // Show the section with the corresponding ID
+    const sectionId = link.getAttribute('href').slice(1);
+    document.getElementById(sectionId).style.display = 'block';
+  });
+});
+
+// Show the first section by default
+links[0].click();
